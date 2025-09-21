dbb0da04e8d39d2b13ce49c468b84db33971986e
iraxit-cloud <indraneel.raxit@icloud.com>
2025-09-21 00:17:43 -0400

    Initial Commit

---
+++ GuideLight v3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GuideLight v3
+//
+//  Created by Indraneel Rakshit on 9/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuideLight v3Tests/GuideLight_v3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  GuideLight_v3Tests.swift
+//  GuideLight v3Tests
+//
+//  Created by Indraneel Rakshit on 9/21/25.
+//
+
+import Testing
+@testable import GuideLight_v3
+
+struct GuideLight_v3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
